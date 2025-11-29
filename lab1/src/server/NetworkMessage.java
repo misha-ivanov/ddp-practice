fa429fe47e7bddefc052adb8ae3cfde2fe5ce2d4
Mike Ivanov <misha.ivanov.ig@gmail.com>
2025-11-30 00:27:44 +0300

    feat: add NetworkMessage
    Signed-off-by: Mike Ivanov <misha.ivanov.ig@gmail.com>

---
+++ lab1/src/server/NetworkMessage.java
@@ -0,0 +1,12 @@
+package server;
+
+public enum NetworkMessage {
+    ROLE, // assign role (X or O)
+    TURN, // assign current player (X or Y)
+    ATTACK, // try change cell (i, j)
+    OK, // if attack is successful (no params)
+    REJECT, // if attack is failed (no params)
+    UPDATE, // update state of cell(i, j)
+    WIN, // assign winner (X or Y)
+    END // declare the end of the game (no params)
+}
