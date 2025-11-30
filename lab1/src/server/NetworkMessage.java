c419c59b35effdc6d98af2beddf955916e9d32fb
Mike Ivanov <misha.ivanov.ig@gmail.com>
2025-11-30 15:12:01 +0300

    feat: add check winner method in Session
    Signed-off-by: Mike Ivanov <misha.ivanov.ig@gmail.com>

--- lab1/src/server/NetworkMessage.java
+++ lab1/src/server/NetworkMessage.java
@@ -4,7 +4,6 @@ public enum NetworkMessage {
     ROLE, // assign role (X or O)
     TURN, // assign current player (X or Y)
     ATTACK, // try change cell (i, j)
-    OK, // if attack is successful (no params)
     REJECT, // if attack is failed (no params)
     UPDATE, // update state of cell(i, j)
     WIN, // assign winner (X or Y)
--- lab1/src/server/Session.java
+++ lab1/src/server/Session.java
@@ -9,11 +9,16 @@ public class Session implements MessageHandler{
     private final ClientConnection c2;
     private final Field field;
 
+    private final int n = 4;
+
     private volatile char currentPlayer = 'X';
     private volatile boolean running = true;
 
     private int movesLeftX;
     private int movesLeftO;
+
+    private int movesDoneX;
+    private int movesDoneO;
 
     public Session(ClientConnection _c1, ClientConnection _c2) {
         c1 = _c1;
@@ -22,10 +27,16 @@ public class Session implements MessageHandler{
         c1.setHandler(this);
         c2.setHandler(this);
 
-        c1.send("X");
-        c2.send("O");
+        c1.send(NetworkMessage.ROLE.name() + " X");
+        c2.send(NetworkMessage.ROLE.name() + " O");
 
-        field = new Field(4);
+        field = new Field(n);
+
+        movesLeftX = 3;
+        movesLeftO = 0;
+
+        movesDoneX = 0;
+        movesDoneO = 0;
     }
 
     @Override
@@ -60,7 +71,7 @@ public class Session implements MessageHandler{
             int row = Integer.parseInt(parts[1]);
             int col = Integer.parseInt(parts[1]);
 
-            // can't place
+            // if can't place
             if(!field.allowPlace(currentPlayer, row, col)){
                 connection.send(NetworkMessage.REJECT.name());
                 return;
@@ -76,6 +87,7 @@ public class Session implements MessageHandler{
                     field.setCellState(row, col, CellState.O_DEAD);
                 }
                 movesLeftX--;
+                movesDoneX++;
             }
             else {
                 if(state == CellState.EMPTY){
@@ -85,11 +97,19 @@ public class Session implements MessageHandler{
                     field.setCellState(row, col, CellState.X_DEAD);
                 }
                 movesLeftO--;
+                movesDoneO++;
             }
 
             // send new updated
             broadcast(NetworkMessage.UPDATE.name() + " " + row + " " + col + " " + field.getCellState(row, col));
 
+            // if someone wins
+            String winner = isSomeoneWins(movesDoneX, movesDoneO);
+            if (winner != null) {
+                broadcast(NetworkMessage.WIN.name() + " " + winner);
+            }
+
+            // turn current player
             if (currentPlayer == 'X' && movesLeftX == 0) {
                 broadcast(NetworkMessage.TURN.name() + " O");
             }
@@ -114,5 +134,60 @@ public class Session implements MessageHandler{
     public void broadcast(String msg) {
         c1.send(msg);
         c2.send(msg);
+    }
+
+    public String isSomeoneWins(int movesDoneX, int movesDoneO) {
+        //check if X wins
+        int cellsChecked = 0;
+        Xbreaker:
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                // if X can attack from X_LIVE
+                if (field.getCellState(i, j) == CellState.X_LIVE &&
+                        (field.hasStateInNeighbourhood(i, j, CellState.EMPTY) ||
+                        field.hasStateInNeighbourhood(i, j, CellState.O_LIVE))) {
+                    break Xbreaker;
+                }
+                // if X can attack from O_DEAD
+                else if (field.getCellState(i, j) == CellState.O_DEAD &&
+                        (field.hasStateInNeighbourhood(i, j, CellState.EMPTY) ||
+                        field.hasStateInNeighbourhood(i, j, CellState.O_LIVE)) &&
+                        field.hasActiveChainInNeighbourhood(i, j, CellState.O_DEAD, CellState.X_LIVE)) {
+                    break Xbreaker;
+                }
+                else {
+                    cellsChecked++;
+                }
+                if (cellsChecked == movesDoneX)
+                    return "O";
+            }
+        }
+
+        //check if O wins
+        cellsChecked = 0;
+        Obreaker:
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                // if O can attack from O_LIVE
+                if (field.getCellState(i, j) == CellState.O_LIVE &&
+                        (field.hasStateInNeighbourhood(i, j, CellState.EMPTY) ||
+                                field.hasStateInNeighbourhood(i, j, CellState.X_LIVE))) {
+                    break Obreaker;
+                }
+                // if O can attack from X_DEAD
+                else if (field.getCellState(i, j) == CellState.X_DEAD &&
+                        (field.hasStateInNeighbourhood(i, j, CellState.EMPTY) ||
+                                field.hasStateInNeighbourhood(i, j, CellState.X_LIVE)) &&
+                        field.hasActiveChainInNeighbourhood(i, j, CellState.X_DEAD, CellState.O_LIVE)) {
+                    break Obreaker;
+                }
+                else {
+                    cellsChecked++;
+                }
+                if (cellsChecked == movesDoneO)
+                    return "X";
+            }
+        }
+        return null;
     }
 }
